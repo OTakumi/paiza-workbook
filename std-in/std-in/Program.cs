2ce3da90b346bc56db41076032de991cfa54ef6d
OTakumi <takumi.o.0410@gmail.com>
2023-01-16 17:58:40 +0900

    Initial commit

---
+++ std-in/std-in/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+	{
+		var s = Console.ReadLine();
+		
+		if(s == "baseball")
+		{
+			Console.WriteLine(9);
+		} else if(s == "soccer")
+		{
+			Console.WriteLine(11);
+		} else if(s == "rugby")
+		{
+			Console.WriteLine(15);
+		} else if(s == "basketball")
+		{
+			Console.WriteLine(5);
+		} else if(s == "volleyball")
+		{
+			Console.WriteLine(6);
+		}
+    }
+}
