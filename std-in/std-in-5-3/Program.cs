4926f8fcb520bb7a26b2445d8c186184461a9344
OTakumi <takumi.o.0410@gmail.com>
2023-01-25 17:53:37 +0900

    add new project

---
+++ std-in/std-in-5-3/Program.cs
@@ -0,0 +1,16 @@
+﻿// 5つの整数の半角スペース区切りの入力 (paizaランク D 相当)
+
+using System;
+
+class Programs
+{
+	static void Main()
+	{
+		var input = Console.ReadLine().Split(' ');
+
+		for(int i=0; i < 5; i++)
+		{
+			Console.WriteLine(input[i]);
+		}
+	}
+}
