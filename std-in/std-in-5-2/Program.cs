cf234cc3d2f81e1ce8c25cb5208c99d056357186
OTakumi <takumi.o.0410@gmail.com>
2023-01-25 17:32:48 +0900

    add new project

---
+++ std-in/std-in-5-2/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2つの整数の半角スペース区切りの入力 (paizaランク D 相当)
+
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		var input = Console.ReadLine().Split(' ');
+		Console.WriteLine(input[0]);
+		Console.WriteLine(input[1]);
+	}
+}
