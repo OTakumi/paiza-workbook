403239ea580a2d240d6ebfa31bcdd58b9ed406b4
OTakumi <takumi.o.0410@gmail.com>
2023-01-26 20:44:07 +0900

    update

---
+++ std-in/std-in-6-4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+		var input = Console.ReadLine().Split(' ');
+
+		for(var i=0; i<n; i++)
+		{
+			Console.WriteLine(input[i]);
+		}
+	}
+}
