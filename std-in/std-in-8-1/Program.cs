e3c60e790db1451977303726c9a72705fee176d8
OTakumi <takumi.o.0410@gmail.com>
2023-02-05 18:15:48 +0900

    add a new project

---
+++ std-in/std-in-8-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		var n = int.Parse(Console.ReadLine());
+
+		for(var i=0; i < n; i++)
+		{
+			var input = Console.ReadLine();
+			Console.WriteLine(input);
+		}
+	}
+}
